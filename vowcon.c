350a96bdac0c58b9d7faab5fca5dbf78388669c4
Harshiq <55382608+HARSHIQGANDHI@users.noreply.github.com>
2020-01-28 12:57:24 +0530

    Add files via upload

---
+++ vowcon.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include<conio.h> 
+ void main()  
+{  
+    char n,a,e,i,o,u;    
+printf("enter the alphabet");    
+scanf("%c",&n);     
+if(n=)
+{    
+printf("it is a vowel");    
+}    
+else
+{    
+printf("it is a  consonant");    
+}     
+    getch();
+    }
